0cfbeee561e1fcde0ce4cd0c398d68fd7fe3378b
Yeiko Nova <YN376061@fi365.ort.edu.uy>
2025-09-02 09:28:40 -0300

    Obscure sensitive configuration details

--- public_html/app/config/config.php
+++ public_html/app/config/config.php
@@ -1,23 +1,23 @@
 <?php
 return [
     'db' => [
-        'host' => 'srv1845.hstgr.io',
-        'name' => 'u995876074_general',
-        'user' => 'u995876074_admin',
-        'pass' => 'Monica17179@',
-        'charset' => 'utf8mb4'
+        'host' => '...',
+        'name' => '...',
+        'user' => '...',
+        'pass' => '...',
+        'charset' => '...'
     ],
     'site' => [
-        'url' => 'https://zetrauy.shop'
+        'url' => '...'
     ],
     'mercadopago' => [
-        'access_token' => 'APP_USR-7285733743417140-030521-3c434f6a99a3c53c45f5561c07634037-400524747',
-        'public_key' => 'APP_USR-945e3348-8661-4b51-ac99-9ef50ea656bf'
+        'access_token' => '...',
+        'public_key' => '...'
     ],
     'whatsapp' => [
-        'token' => 'EAAPg9ca6EwwBPSepWUrEsImCShEMz7DG6ReBclFzBXhRMr6TCWFXBePJ5yNq4gllfttIYhIKuDS9LQahpzwcRfSiKIudH70VvT0TaFgHZC2xWnrGzSRv5Yc3rHX5ZByJr16i1xcIcRgEz8codlH7qX9cnhn1fyOMyoUE5Vd6dORZCZAGKSPvh24vlJnkZAYFB1TZBqT8eZCojMQ9WDD0wOtkAZBPCM2cdLirDLImqIJdCoSj979n8goJlm6xCUITEwZDZD',
-        'phone_number_id' => '710692248800192'
+        'token' => '...',
+        'phone_number_id' => '...'
     ],
-    'base_url' => 'https://zetrauy.shop'
+    'base_url' => '...'
 ];
 
